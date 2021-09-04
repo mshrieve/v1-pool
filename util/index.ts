f01b3e83a9c32d7964a07882039a61638c1871b9
mshrieve <mshrieve@gmail.com>
2021-09-04 14:58:47 -0700

    update util swap function

--- util/index.ts
+++ util/index.ts
@@ -1,6 +1,7 @@
 import { BigNumber } from 'ethers'
 
 const one = BigNumber.from(10).pow(18)
+// feeRate = .03, with 18 decimals
 const feeRate = BigNumber.from(10).pow(16).mul(3)
 
 const computeExpectedEthReceived = (
@@ -15,17 +16,14 @@ const computeExpectedTokensReceived = (
   amount: BigNumber
 ) => swapXforY(ethPool, tokenPool, amount)
 
-const swapXforY = (x: BigNumber, y: BigNumber, amount: BigNumber) => {
+const swapXforY = (x: BigNumber, y: BigNumber, dx: BigNumber) => {
   // compute invariant
   const k = x.mul(y)
-  // feeRate = .03
-  const fee = amount.mul(feeRate).div(one)
-  // update x
-  x = x.add(amount)
-  // yOut = y - k / (x - fee)
-  const yOut = y.sub(k.div(x.sub(fee)))
-
-  return yOut
+  // compute fee
+  const fee = dx.mul(feeRate).div(one)
+  // dy = y - k / (x + dx - fee)
+  const dy = y.sub(k.div(x.add(dx).sub(fee)))
+  return dy
 }
 
 export { computeExpectedTokensReceived, computeExpectedEthReceived }
